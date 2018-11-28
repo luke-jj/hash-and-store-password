7033c92c98baa0d6a638a50ba9a146e5e0cd2ca9
infi <infiLuca@gmail.com>
2018-11-28 18:19:12 +0100

    start sha function

--- src/main.c
+++ src/main.c
@@ -33,7 +33,7 @@
 #define ROWS_MAX 100
 
 /*
- * Simple demo program that demonstrates the hash of a user-password
+ * Simple demo program that demonstrates hashing and storing a user-password.
  *
  * openssl instructions:
  * 1. download and install the latest version from https://www.openssl.org
@@ -62,12 +62,28 @@ void clear_screen()
     printf("\033[H\033[2J");
 }
 
+/*
+ *
+ */
+unsigned char hash_password(char *password)
+{
+    SHA256_CTX context;
+    unsigned char final_hash[SHA256_DIGEST_LENGTH];
+
+    SHA256_Init(&context);
+    SHA256_Update(&context, password, strlen(password));
+    SHA256_Final(final_hash, &context);
+
+    return final_hash;
+}
+
 int register_user()
 {
     char    username[256];
     char    password[256];
     int     username_is_valid = 0;
     int     password_is_valid = 0;
+    int     is_alnum;
     int     i;
 
     while (!username_is_valid) {
@@ -75,12 +91,16 @@ int register_user()
         printf("Please enter a valid username (A-Za-z0-9, max. 10 characters)");
         printf("\n >  ");
         scanf("%s", username);
-        if (strlen(username) > 10) {
-            for (i = 0; i < strlen(username); i++) {
-                if (!isalnum(username[i]) ) {
-                    printf("not a valid username");
-                }
+        is_alnum = 1;
+
+        /* check that username is shorter than 11 characters and alphanumeric */
+        for (i = 0; i < strlen(username); i++) {
+            if (!isalnum(username[i])) {
+                is_alnum = 0;
             }
+        }
+        if (strlen(username) < 10 && is_alnum) {
+            username_is_valid = 1;
         }
 
     }
@@ -90,20 +110,33 @@ int register_user()
         printf("Please enter a valid password (A-Za-z0-9, max. 8 characters)");
         printf("\n >  ");
         scanf("%s", password);
+        is_alnum = 1;
+
+        /* check that password is shorter than 9 characters and alphanumeric */
+        for (i = 0; i < strlen(password); i++) {
+            if (!isalnum(password[i])) {
+                is_alnum = 0;
+            }
+        }
+        if (strlen(password) < 10 && is_alnum) {
+            password_is_valid = 1;
+        }
     }
     if (username_is_valid && password_is_valid) {
+        hash_password(password);
+
         printf("Success! You can now login with username: %s\n", username);
     }
 
     return 0;
 }
 
-int check_password()
+int login()
 {
     return 0;
 }
 
-int main(int argc, char *argv[])
+void main_menu()
 {
     char *login_name = "--";
     char user_input;
@@ -122,7 +155,7 @@ int main(int argc, char *argv[])
                 register_user();
                 break;
             case 'l':
-                check_password();
+                login();
                 break;
             case 'q':
                 exit(0);
@@ -131,6 +164,11 @@ int main(int argc, char *argv[])
                 scanf(" %c", &user_input);
         }
     }
+}
+
+int main(int argc, char *argv[])
+{
+    main_menu();
 
     return 0;
 }
